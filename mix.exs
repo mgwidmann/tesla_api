5058623f1d97a44de7accecdcda075465c1ff9e1
Adrian Kumpf <adrian.kumpf@posteo.de>
2019-07-19 13:04:02 +0200

    Upgrade dependencies

--- mix.exs
+++ mix.exs
@@ -13,17 +13,15 @@ defmodule TeslaApi.MixProject do
 
   # Run "mix help compile.app" to learn about applications.
   def application do
-    [
-      extra_applications: [:logger]
-    ]
+    []
   end
 
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:tesla, "~> 1.2.0"},
-      {:hackney, "~> 1.14.0"},
-      {:jason, ">= 1.0.0"}
+      {:tesla, "~> 1.2"},
+      {:hackney, "~> 1.14"},
+      {:jason, "~> 1.0"}
     ]
   end
 end
